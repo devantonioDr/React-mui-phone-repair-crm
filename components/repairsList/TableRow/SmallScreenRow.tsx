efec798c712736b171610393b4ce0aea9e2caeeb
devantonioDr <109156169+devantonioDr@users.noreply.github.com>
2022-07-19 06:48:02 +0800

    refactored RowDelete Context and removed all ramdomBackground colors

---
+++ components/repairsList/TableRow/SmallScreenRow.tsx
@@ -0,0 +1,80 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import TableCell from "@mui/material/TableCell";
+import Typography from "@mui/material/Typography";
+import { withContextSelectRowCheckBox } from "../context/RowSelectContext";
+import { withContextShowMore } from "../context/RowShowMoreContext";
+import { StatusRow } from "../StatusRow";
+import TableRowActions from "./actionsUi";
+import { InfoIdButton, SelectRowCheckBox, ShowMoreButton } from "./commonUi";
+
+// Assign context for SelectRowCheckBox actions
+const SelectRowCheckBoxWithContext =
+  withContextSelectRowCheckBox(SelectRowCheckBox);
+
+// Assign context for ShowMore Row actions
+const ShowMoreButtonWithContext = withContextShowMore(ShowMoreButton);
+
+
+
+function SmallScreenRowCell({ title, value }: any) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="subtitle2">{title}</Typography>
+      <span>{value}</span>
+    </Box>
+  );
+}
+
+export const SmallScreenRowContent = ({ data }: { data: RepairData }) => {
+  let { invoiceId, status, branchOffice, admissionDate, device, customer } =
+    data;
+  return (
+    <>
+      <TableCell padding="none" align="left">
+        <Stack padding={2} spacing={3}>
+          <SmallScreenRowCell
+            title=""
+            value={<SelectRowCheckBoxWithContext invoiceId={invoiceId} />}
+          />
+
+          <SmallScreenRowCell
+            title="Folio:"
+            value={<InfoIdButton invoiceId={invoiceId} />}
+          />
+          <SmallScreenRowCell title="IMEI:" value={""} />
+
+          <SmallScreenRowCell title="Fecha de ingreso:" value={admissionDate} />
+
+          <SmallScreenRowCell title="Marca:" value={device.trademark} />
+
+          <SmallScreenRowCell title="Modelo:" value={device.model} />
+
+          <SmallScreenRowCell title="Cliente:" value={customer.name} />
+
+          <SmallScreenRowCell
+            title="Estado:"
+            value={<StatusRow status={status.key} />}
+          />
+
+          <SmallScreenRowCell
+            title="Sucursal:"
+            value={<Chip label={branchOffice.name} />}
+          />
+          <SmallScreenRowCell title="Acciones:" value={<TableRowActions />} />
+
+          <ShowMoreButtonWithContext invoiceId={invoiceId} />
+        </Stack>
+      </TableCell>
+    </>
+  );
+};
